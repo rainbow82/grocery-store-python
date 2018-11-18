333f559f7fa18198b2f4353f486048d07e2b0a94
Shannon Beach <shannonbeach1982@gmail.com>
2018-11-18 13:08:19 -0500

    calculate total grocery bill, adjust inventory

--- script.py
+++ script.py
@@ -12,14 +12,27 @@ stock={
     "pear": 15
 }
 
+#print stock list
 for key in prices:
     print key
     print "price %s" % prices[key]
     print "stock %s" % stock[key]
 
+#total that could be made 
 total = 0
 for key in prices:
   print prices[key] * stock[key]
   temp_total = prices[key] * stock[key]
   total = total + temp_total
 print total
+
+#total grocery list and reduce inventory
+def compute_bill(food):
+  total = 0
+  for item in food:
+    if stock[item] > 0:
+      total += prices[item]
+      stock[item] -= 1
+  return total
+
+shopping_list = ["banana", "orange", "apple"]
